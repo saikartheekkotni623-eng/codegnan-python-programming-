41fd4e981ed2e489844fb6da8004d010191b2edd
saikartheekkotni623-eng <saikartheekkotni623@gmail.com>
2026-09-24 17:24:56 +0530

    Add files via upload

---
+++ negetive sliding.py
@@ -0,0 +1,20 @@
+Python 3.14.7 (tags/v3.14.7:823f032, Aug  5 2026, 10:51:32) [MSC v.1944 64 bit (AMD64)] on win32
+Enter "help" below or click "Help" above for more information.
+>>> #negetive slicing
+>>> a="python course"
+>>> a[-1:-8:-2]
+'ero '
+>>> a[-2:-12:-3]
+'sont'
+>>> a[-4:-13:-4]
+'uny'
+>>> a[6:2:1]
+''
+>>> a[-8:-4:-2] #not possible
+''
+a[-4:-8:-2]
+'uc'
+a[::1]
+'python course'
+a[::-1]
+'esruoc nohtyp'
